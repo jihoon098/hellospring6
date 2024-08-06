968fa5f8340e972a3b051c7b36be872d05c85e11
jihoon098 <jihoon098@naver.com>
2024-08-07 00:37:58 +0900

    상속을 활용하여 확장성있는 코드로 개선

---
+++ src/main/java/hoonspring/hellospring6/WebApiExRatePaymentService.java
@@ -0,0 +1,34 @@
+package hoonspring.hellospring6;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService{
+
+    @Override
+    BigDecimal getExRate(String currency) throws IOException {
+
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        /*
+         * 1. InputStream : 파일 or 네트워크에서 넘어오는 데이터를 Byte 형태로 리턴.
+         * 2. InputStreamReader : Byte 데이터를 사람이 읽을 수 있는 문자(Character)로 변환하여 리턴.
+         * 3. BufferedReader : Character는 단순한 문자의 나열이기 때문에, 읽기 편하게 텍스트로 변환하여 리턴.
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        return exRate;
+    }
+}
