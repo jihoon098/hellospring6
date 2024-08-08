c73548d50d3e56781b76e1a2a46585645d8df520
jihoon098 <jihoon098@naver.com>
2024-08-08 20:20:14 +0900

    Spring Container(BeanFactory) 도입

--- src/main/java/hoonspring/hellospring6/Client.java
+++ src/main/java/hoonspring/hellospring6/Client.java
@@ -1,14 +1,21 @@
 package hoonspring.hellospring6;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
-        
+        /*
+         * 스프링인 BeanFactory는 어플리케이션 구성정보를 모름.
+         * 어플리케이션의 구성정보를 스프링컨테이너가 사용하게 해줘야함.
+         */
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = (PaymentService) beanFactory.getBean(PaymentService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/hoonspring/hellospring6/ObjectFactory.java
+++ src/main/java/hoonspring/hellospring6/ObjectFactory.java
@@ -1,11 +1,22 @@
 package hoonspring.hellospring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * @Configuration
+ * : 기존 평범한 클래스인 ObjectFactory가 '구성 정보'를 가지고 있는 클래스다라는 것을 스프링에게 알려주는 어노테이션.
+ *   말그대로 Config정보를 가지고있는 클래스를 의미.
+ */
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider(){
         return new WebApiExRateProvider();
     }
