f9880f181b9ffb8b3f5a7b114b4e996f07f114e7
jihoon098 <jihoon098@naver.com>
2024-08-08 23:54:12 +0900

    @Component와 @ComponentScan

--- src/main/java/hoonspring/hellospring6/ObjectFactory.java
+++ src/main/java/hoonspring/hellospring6/ObjectFactory.java
@@ -1,16 +1,22 @@
 package hoonspring.hellospring6;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /*
  * @Configuration
  * : 기존 평범한 클래스인 ObjectFactory가 '구성 정보'를 가지고 있는 클래스다라는 것을 스프링에게 알려주는 어노테이션.
  *   말그대로 Config정보를 가지고있는 클래스를 의미.
+ *
+ * @ComponentScan
+ * : @Component가 붙은 클래스를 찾아서 오브젝트를 생성하고 의존을 주입하는 역할을 스프링이 내부적으로 하게 만듬.
+ *   스프링이 필요로 하는 것은 '빈과 관련된 정보'
  */
 @Configuration
+@ComponentScan
 public class ObjectFactory {
 
+    /*
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
@@ -20,4 +26,5 @@ public class ObjectFactory {
     public ExRateProvider exRateProvider(){
         return new WebApiExRateProvider();
     }
+    */
 }
