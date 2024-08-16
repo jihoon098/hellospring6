fee09daaf5c1310dcae3d99775cb9e83f954a321
jihoon098 <jihoon098@naver.com>
2024-08-16 15:06:28 +0900

    Template 오브젝트 분리(ApiTemplate)

---
+++ src/main/java/hoonspring/hellospring6/api/ApiTemplate.java
@@ -0,0 +1,34 @@
+package hoonspring.hellospring6.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ApiTemplate {
+
+    // 메소드 추출(Api를 호출하여 환율정보를 가져오는 기본적인 틀 분리)
+    public BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            response = apiExecutor.execute(uri);
+        } catch(IOException e) {
+            throw new RuntimeException();
+        }
+
+        try {
+            return exRateExtractor.extract(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/hoonspring/hellospring6/exRate/WebApiExRateProvider.java
+++ src/main/java/hoonspring/hellospring6/exRate/WebApiExRateProvider.java
@@ -1,50 +1,26 @@
 package hoonspring.hellospring6.exRate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import hoonspring.hellospring6.api.ApiExecutor;
+import hoonspring.hellospring6.api.ApiTemplate;
 import hoonspring.hellospring6.api.ErApiExRateExtractor;
-import hoonspring.hellospring6.api.ExRateExtractor;
+import hoonspring.hellospring6.api.HttpClientApiExecutor;
 import hoonspring.hellospring6.api.SimpleApiExecutor;
 import hoonspring.hellospring6.payment.ExRateProvider;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Component
 public class WebApiExRateProvider implements ExRateProvider {
+
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiforExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
-
-    // 메소드 추출(Api를 호출하여 환율정보를 가져오는 기본적인 틀 분리)
-    private static BigDecimal runApiforExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch(IOException e) {
-            throw new RuntimeException();
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
