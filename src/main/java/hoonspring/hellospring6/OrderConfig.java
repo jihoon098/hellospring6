adc3c94ace4e2e64cb573fcbfc7445631596583b
jihoon098 <jihoon098@naver.com>
2024-08-22 17:23:18 +0900

    스프링의 '서비스 추상화' 적용하여 개선2 (JpaTransactionManager 분리)

--- src/main/java/hoonspring/hellospring6/OrderConfig.java
+++ src/main/java/hoonspring/hellospring6/OrderConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @Import(DataConfig.class)
@@ -18,7 +19,7 @@ public class OrderConfig {
     }
 
     @Bean
-    public OrderService orderService(JpaTransactionManager transactionManager) {
+    public OrderService orderService(PlatformTransactionManager transactionManager) {
         return new OrderService(orderRepository(), transactionManager);
     }
 }
