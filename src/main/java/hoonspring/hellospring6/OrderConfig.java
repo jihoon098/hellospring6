d21e13ca4f0e12039beaf609b19ed696f1728dbd
jihoon098 <jihoon098@naver.com>
2024-08-24 13:52:30 +0900

    @transactional 적용

--- src/main/java/hoonspring/hellospring6/OrderConfig.java
+++ src/main/java/hoonspring/hellospring6/OrderConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
 
     @Bean
@@ -18,7 +20,7 @@ public class OrderConfig {
     }
 
     @Bean
-    public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 }
--- src/main/java/hoonspring/hellospring6/data/OrderServiceImpl.java
+++ src/main/java/hoonspring/hellospring6/data/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package hoonspring.hellospring6.data;
 import hoonspring.hellospring6.order.Order;
 import hoonspring.hellospring6.order.OrderReq;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,6 +18,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public Order createOrder(String no, BigDecimal total) {
         Order order = new Order(no, total);
 
@@ -25,6 +27,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public List<Order> createOrders(List<OrderReq> reqs) {
         return reqs.stream().map(req -> createOrder(req.no(), req.total())).toList();
     }
